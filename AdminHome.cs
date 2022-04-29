c14a216f62e2bfb7e98cdcac9c786a39073e621f
Bob <masonho8273@gmail.com>
2022-04-29 14:32:34 -0400

    Commit admin home

--- AdminHome.cs
+++ AdminHome.cs
@@ -10,11 +10,28 @@ using System.Windows.Forms;
 
 namespace NoahHoMau
 {
+    //todo rene check this for me.
     public partial class AdminHome : Form
     {
         public AdminHome()
         {
             InitializeComponent();
+        }
+
+        //This method will handle the order manager button being pressed and will take the user to the order manager.
+        private void btn_OrderManager_Click(object sender,EventArgs e)
+        {
+            AdminOrders nav_Admin_Orders = new AdminOrders();
+            nav_Admin_Orders.Show();
+            this.Hide();
+        }
+
+        //This method will handle the customer manager being pressed and will take the user to the customer manager.
+        private void btn_CustomerManager_Click(object sender,EventArgs e)
+        {
+            AdminCustomers nav_Admin_Customer = new AdminCustomers();
+            nav_Admin_Customer.Show();
+            this.Hide();
         }
     }
 }
